a82a6cfa18ee52acc9e4f5b94bd7b3886f00c90c
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-05-18 20:58:44 -0400

    Initial Commit

---
+++ SPoT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SPoT
+//
+//  Created by Hamdan Javeed on 2013-05-18.
+//  Copyright (c) 2013 Glass Cube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SPoT/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SPoT
+//
+//  Created by Hamdan Javeed on 2013-05-18.
+//  Copyright (c) 2013 Glass Cube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
