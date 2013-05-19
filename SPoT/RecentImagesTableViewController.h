215cec71c95a05e84e1bd391f6c3b48aa7671cc1
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-05-19 11:52:34 -0400

    Feature-Required Task 2: created two UITableViewControllers
    Created two table views and hooked them up to the tab bar controller.
    Embeded the two view controllers in a navigation controller.

---
+++ SPoT/RecentImagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecentImagesTableViewController.h
+//  SPoT
+//
+//  Created by Hamdan Javeed on 2013-05-19.
+//  Copyright (c) 2013 Glass Cube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecentImagesTableViewController : UITableViewController
+
+@end
