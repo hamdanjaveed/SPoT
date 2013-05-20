4ec15fca7264945ef7cb32e3ed8f46a3c832e589
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-05-20 04:22:04 -0400

    Feature-Required Task 3: obtains unique tags

--- SPoT/FlickrImagesTableViewController.m
+++ SPoT/FlickrImagesTableViewController.m
@@ -7,12 +7,38 @@
 //
 
 #import "FlickrImagesTableViewController.h"
+#import "FlickrFetcher.h"
 
 @interface FlickrImagesTableViewController ()
-
+@property (strong, nonatomic) NSMutableArray *uniqueTags;
 @end
 
 @implementation FlickrImagesTableViewController
+
+- (NSMutableArray *)uniqueTags {
+    if (!_uniqueTags) {
+        _uniqueTags = [[NSMutableArray alloc] init];
+    }
+    return _uniqueTags;
+}
+
+- (void)viewDidLoad {
+    NSArray *pics = [[NSArray alloc] initWithArray:[FlickrFetcher stanfordPhotos]];
+    for (NSDictionary *dictionary in pics) {
+        NSString *tags = [dictionary objectForKey:FLICKR_TAGS];
+        NSArray *tagArray = [tags componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@" "]];
+        for (NSString *tag in tagArray) {
+            if (![self.uniqueTags containsObject:tag] && ![[FlickrImagesTableViewController invalidTags] containsObject:tag]) {
+                [self.uniqueTags addObject:tag];
+            }
+        }
+    }
+    NSLog(@"%@", self.uniqueTags);
+}
+
++ (NSArray *)invalidTags {
+    return @[@"cs193pspot", @"portrait", @"landscape"];
+}
 
 #pragma mark - Table view data source
 
